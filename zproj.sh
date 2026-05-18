34cfc25674b9632ef5c1602cb04cea8a02a53944
Okuda Ritsu (奥田律) <okudaritsu22@gmail.com>
2026-05-18 21:22:58 +0200

    Add License option
    After creating a Project, it will ask you to add a License. Now it support MIT-License, GLP 3.0, Apache 2.0, MPL 2.0 and no License.

--- zproj.sh
+++ zproj.sh
@@ -20,9 +20,10 @@ elif [ -d "$pdir" ]; then
     exit 3
 fi
 
+# 1. Template auswählen
 cur="$PWD"
 cd $tdir
-echo "Please select a template"
+echo "Please select a template:"
 select x in *; do
     template="$x"
     break
@@ -31,15 +32,66 @@ done
 cd $cur
 cp -R ${tdir}/$template $pdir
 cd $pdir
-for x in *; do
+
+# 2. Lizenz auswählen (Inklusive MPL-2.0)
+echo "---------------------------------------"
+echo "Please select a LICENSE for your project:"
+options=("MIT" "Apache-2.0" "GPL-3.0" "MPL-2.0" "None")
+select lic in "${options[@]}"; do
+    case $lic in
+        "MIT")
+            echo "Generating MIT License..."
+            year=$(date +%Y)
+            author=$(git config user.name 2>/dev/null || echo "[Your Name]")
+            echo "Copyright (c) $year $author" > LICENSE
+            echo "" >> LICENSE
+            echo "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" >> LICENSE
+            echo "" >> LICENSE
+            echo "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software." >> LICENSE
+            echo "" >> LICENSE
+            echo "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE." >> LICENSE
+            break
+            ;;
+        "Apache-2.0")
+            echo "Downloading Apache 2.0 License..."
+            curl -s https://www.apache.org/licenses/LICENSE-2.0.txt > LICENSE
+            break
+            ;;
+        "GPL-3.0")
+            echo "Downloading GPL v3 License..."
+            curl -s https://www.gnu.org/licenses/gpl-3.0.txt > LICENSE
+            break
+            ;;
+        "MPL-2.0")
+            echo "Downloading Mozilla Public License 2.0..."
+            curl -s https://www.mozilla.org/MPL/2.0/index.txt > LICENSE
+            break
+            ;;
+        "None")
+            echo "Skipping License generation."
+            break
+            ;;
+        *)
+            echo "Invalid option $REPLY";;
+    esac
+done
+echo "---------------------------------------"
+
+# 3. Platzhalter ersetzen
+find . -type f | while read -r x; do
+    if [ "$(basename "$x")" = "LICENSE" ]; then continue; fi
+
     new=$(sed "s,PROJECTNAME,$pname,g" <<< "$x")
     if [ "$x" = "$new" ]; then
-        sed "s,PROJECTNAME,$pname,g" < $x > temp
-        mv -f temp $x
+        sed "s,PROJECTNAME,$pname,g" < "$x" > temp
+        mv -f temp "$x"
     else
-        sed "s,PROJECTNAME,$pname,g" < $x > $new
+        mkdir -p "$(dirname "$new")"
+        sed "s,PROJECTNAME,$pname,g" < "$x" > "$new"
         if [ -e "$new" ]; then
-            rm -f $x
+            rm -f "$x"
         fi
     fi
 done
+
+echo "Project $pname successfully created!"
