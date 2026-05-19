6816a54ff7cbd50c36c319cbc02b791f9e99f4f1
Okuda Ritsu (奥田律) <okudaritsu22@gmail.com>
2026-05-19 18:01:17 +0200

    Update zproj.sh

--- zproj.sh
+++ zproj.sh
@@ -1,52 +1,45 @@
 #!/bin/bash
 
-tdir="/var/zproj/templates"
-ddir="/var/zproj/default"
-cur="$PWD"
-
 usage() {
-    >&2 echo "Usage:"
-    >&2 echo "  $0 <projectname>  - create new Project"
+    >&2 echo "Usage: $0 <project>"
     return 0
 }
-if ! [ -d "$tdir" ]; then
+
+pname=$1
+pdir="./${pname}"
+tdir=/var/zproj/templates
+
+if [ -z "$pname" ]; then
+    usage
+    exit 1
+elif ! [ -d "$tdir" ]; then
     >&2 echo "Unable to find template dir: $tdir"
     exit 2
+elif [ -d "$pdir" ]; then
+    >&2 echo "Project dir already exists: $pdir"
+    exit 3
 fi
-        pname=$1
-        pdir="./${pname}"
 
-        if [ -d "$pdir" ]; then
-            >&2 echo "Project dir already exists: $pdir"
-            exit 3
+cur="$PWD"
+cd $tdir
+echo "Please select a template"
+select x in *; do
+    template="$x"
+    break
+done
+
+cd $cur
+cp -R ${tdir}/$template $pdir
+cd $pdir
+for x in *; do
+    new=$(sed "s,PROJECTNAME,$pname,g" <<< "$x")
+    if [ "$x" = "$new" ]; then
+        sed "s,PROJECTNAME,$pname,g" < $x > temp
+        mv -f temp $x
+    else
+        sed "s,PROJECTNAME,$pname,g" < $x > $new
+        if [ -e "$new" ]; then
+            rm -f $x
         fi
-
-        cd "$tdir" || exit 1
-        echo "Please select a template:"
-        select x in *; do
-            if [ -n "$x" ]; then
-                template="$x"
-                break
-            fi
-        done
-
-        cd "$cur" || exit 1
-        cp -R "${tdir}/$template" "$pdir"
-        cd "$pdir" || exit 1
-        for x in *; do
-            if [ -f "$x" ]; then
-                new=$(sed "s,PROJECTNAME,$pname,g" <<< "$x")
-                if [ "$x" = "$new" ]; then
-                    sed "s,PROJECTNAME,$pname,g" < "$x" > temp
-                    mv -f temp "$x"
-                else
-                    sed "s,PROJECTNAME,$pname,g" < "$x" > "$new"
-                    if [ -e "$new" ]; then
-                        rm -f "$x"
-                    fi
-                fi
-            fi
-        done
-        echo "Project $pname successfully created!"
-        ;;
-esac
+    fi
+done
