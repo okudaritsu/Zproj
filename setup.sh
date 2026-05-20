59479aed1753af702b5a882e9e4530ca2a28433f
Okuda Ritsu (奥田律) <okudaritsu22@gmail.com>
2026-05-20 19:23:55 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Let's install Zproj."
-
-sudo cp -f zproj.sh /usr/local/bin
-sudo mkdir -p /var/zproj
-sudo cp -rf templates /var/zproj
-
-echo "installed! Now type zproj.sh and use it."
